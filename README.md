889c300ff762178717b6d7e70c7301dcd71449dc
ninad hatkar <ninadh22@gmail.com>
2017-10-03 18:58:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# This is dummy to test packagist 
+
+Ignore it
+
+## License
+This project is licensed under MIT license
---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Dynamo;
+
+
+abstract class Dynamo {
+    public function __construct($api_key = '', $access_token = '', $environment = 'development') {
+        $this->say = "hello world"
+        return $this;
+    }
